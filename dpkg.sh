2ea846cf70f3f6d98f86e3ac7cefd3772588b00f
estebah <sleepycomputing@outlook.com>
2023-06-23 14:58:25 -0600

    Update dpkg.sh

--- dpkg.sh
+++ dpkg.sh
@@ -1,26 +1,20 @@
 #! /bin/sh
 
 echo "\e[31mฅ^•ﻌ•^ฅ...\e[0m"
+
 sudo apt update -y && sudo apt full-upgrade
-sudo apt install zsh
 sudo apt install xz-utils
 sudo apt install curl
 sudo apt install git
-sudo apt install wget
-sudo apt install python3 python3-pip
 sudo apt install build-essential
 sudo apt install ripgrep
 sudo apt install fd-find
 sudo apt install tree
 sudo apt install tmux
 sudo apt install htop
-sudo apt install zip
 sudo apt install hexedit
 sudo apt install neofetch
-sudo apt install zip unzip
 sudo apt install ninja-build
-sudo apt install gettext
 sudo apt install cmake
-sudo apt install clang-13
-sudo apt install man-db
+sudo apt install clang
 sudo apt install net-tools
