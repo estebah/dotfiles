97c68616e165e19d6ece1704c4b9bda0c450da37
estebah <sleepycomputing@outlook.com>
2023-07-07 03:42:10 -0600

    Improve dpkg.sh

--- dpkg.sh
+++ dpkg.sh
@@ -2,18 +2,22 @@
 
 echo "\e[31mฅ^•ﻌ•^ฅ...\e[0m"
 
-sudo apt update -y && sudo apt full-upgrade
-sudo apt install curl
-sudo apt install git
-sudo apt install build-essential
-sudo apt install ripgrep
-sudo apt install fd-find
-sudo apt install tree
-sudo apt install tmux
-sudo apt install htop
-sudo apt install hexedit
-sudo apt install neofetch
-sudo apt install ninja-build
-sudo apt install cmake
-sudo apt install clang
-sudo apt install net-tools
+dpkg="curl
+git
+build-essential
+ripgrep
+fd-find
+tree
+tmux
+htop
+hexedit
+neofetch
+ninja-build
+cmake
+clang
+net-tools"
+
+for n in $dpkg;
+do
+  sudo apt install -y $n
+done
